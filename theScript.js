514c426e8719bd256d3a2ea869e85fd1339805b5
nsmtetwa <115722285+nsmtetwa@users.noreply.github.com>
2024-02-14 10:24:48 +0200

    Create theScript.js

---
+++ theScript.js
@@ -0,0 +1,9 @@
+window.foo = {
+    bar: function() {
+        console.log("Hello from foo.bar function!");
+        // Add your functionality here
+    }
+};
+
+// Optionally, you can invoke the function immediately after defining it
+window.foo.bar();
