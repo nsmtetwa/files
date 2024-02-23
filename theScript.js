67d4f66ed14b96696fa964224c45f17ae8e8025e
nsmtetwa <115722285+nsmtetwa@users.noreply.github.com>
2024-02-23 10:12:57 +0200

    Update and rename theScript.js to item_uri.json

--- theScript.js
+++
@@ -1,9 +0,0 @@
-window.foo = {
-    bar: function() {
-        console.log("Hello from foo.bar function!");
-        // Add your functionality here
-    }
-};
-
-// Optionally, you can invoke the function immediately after defining it
-window.foo.bar();
